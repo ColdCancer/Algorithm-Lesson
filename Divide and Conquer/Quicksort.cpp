42439c42fbfab70dfa1f484f6464010b43a8215d
ColdCancer <svipvm@yeah.net>
2020-09-17 18:38:18 +0800

    modify Quicksort.cpp

--- Divide and Conquer/Quicksort.cpp
+++ Divide and Conquer/Quicksort.cpp
@@ -4,7 +4,8 @@ void Quicksort(int* a, int x, int y)
 {
     if(y <= x) return;
     int i = x - 1, j = y + 1, num = a[x];
-    while(i < j) {
+    while(i < j) 
+    {
         while(a[++i] < num);
         while(num < a[--j]);
         if(i < j) swap(a[i], a[j]);
@@ -16,9 +17,13 @@ int main()
 {
     int n = N, a[N];
     for(int i = 0; i < n; i++)
+    {
         scanf("%d", &a[i]);
+    }
     Quicksort(a, 0, n - 1);
     for(int i = 0; i < n; i++)
+    {
         printf("%d ", a[i]);
+    }
     return 0;
 }

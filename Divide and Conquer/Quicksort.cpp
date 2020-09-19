1dbfdbc00899ebf191dd092997a31ba3431c737a
ColdCancer <svipvm@yeah.net>
2020-09-19 12:31:26 +0800

    modify QuickSort.cpp

--- Divide and Conquer/Quicksort.cpp
+++ Divide and Conquer/Quicksort.cpp
@@ -1,11 +1,9 @@
 # include "myfac.h"
 # define N 10
-void Quicksort(int* a, int x, int y)
-{
+void Quicksort(int* a, int x, int y) {
     if(y <= x) return;
     int i = x - 1, j = y + 1, num = a[x];
-    while(i < j) 
-    {
+    while(i < j)  {
         while(a[++i] < num);
         while(num < a[--j]);
         if(i < j) swap(a[i], a[j]);
@@ -13,16 +11,13 @@ void Quicksort(int* a, int x, int y)
     Quicksort(a, x, j);
     Quicksort(a, j + 1, y);
 }
-int main()
-{
+int main() {
     int n = N, a[N];
-    for(int i = 0; i < n; i++)
-    {
+    for(int i = 0; i < n; i++) {
         scanf("%d", &a[i]);
     }
     Quicksort(a, 0, n - 1);
-    for(int i = 0; i < n; i++)
-    {
+    for(int i = 0; i < n; i++) {
         printf("%d ", a[i]);
     }
     return 0;

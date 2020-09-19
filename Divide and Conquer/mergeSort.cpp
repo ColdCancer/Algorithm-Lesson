e72b8913028c8b30e77d23c5f7b63ad8622649ef
ColdCancer <svipvm@yeah.net>
2020-09-19 12:30:47 +0800

    modify MergeSort.cpp

--- Divide and Conquer/mergeSort.cpp
+++ Divide and Conquer/mergeSort.cpp
@@ -1,30 +1,24 @@
 # include "myfac.h"
 # define N 10
-void mergeSort(int* a, int *b, int x, int y)
-{
+void mergeSort(int* a, int *b, int x, int y) {
     if(y <= x) return;
     int i = x, m = (x + y) / 2, j = m + 1;
     mergeSort(a, b, x, m);
     mergeSort(a, b, j, y);
-    for(int k = x; k <= y; k++)
-    {
+    for(int k = x; k <= y; k++) {
         if(y < j || (i <= m && a[i] <= a[j]))
             b[k] = a[i++];
-        else
-            b[k] = a[j++];
+        else b[k] = a[j++];
     }
     for(int k = x; k <= y; k++) a[k] = b[k];
 }
-int main()
-{
+int main() {
     int n = N, a[N], b[N];
-    for(int i = 0; i < n; i++)
-    {
+    for(int i = 0; i < n; i++) {
         scanf("%d", &a[i]);
     }
     mergeSort(a, b, 0, n - 1);
-    for(int i = 0; i < n; i++)
-    {
+    for(int i = 0; i < n; i++) {
         printf("%d ", a[i]);
     }
     return 0;
